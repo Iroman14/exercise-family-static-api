59d7bea243dd19c3e7e1b689adfca20c1d9746fa
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2025-04-09 11:29:10 +0000

    Proyecto Final

--- src/app.py
+++ src/app.py
@@ -1,45 +1,63 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
-# from models import Person
-
+from datastructures import Family
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# Create the jackson family object
-jackson_family = FamilyStructure("Jackson")
+jackson_family = Family("Jackson")
 
-
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 
-# Generate sitemap with all your endpoints
-@app.route('/')
+@app.route("/")
 def sitemap():
     return generate_sitemap(app)
 
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+@app.route("/members", methods=["GET"])
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    if members is not None:
+        print(members)
+        return jsonify(members), 200
+    else:
+        return jsonify({'error': 'Miembros no encotrados'}), 400
 
 
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member is not None:
+        return jsonify(member), 200
+    else:
+        return jsonify({'error': 'Miembro no encotrado'}), 400
 
-# This only runs if `$ python src/app.py` is executed
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.get_member(id)
+    if member is not None:
+        member = jackson_family.delete_member(id)
+        return jsonify({'done':True}), 200
+    else:
+        return jsonify({'error': 'Miembro no encotrado'}), 400
+
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    member = request.json
+    if member is not None:
+        new_member = jackson_family.add_member(member)
+        return jsonify(new_member), 200
+    else:
+        return jsonify({'error': 'Información Invalida'}), 400
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
